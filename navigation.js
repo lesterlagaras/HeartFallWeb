a4add188a2576e8e45a197a846942bde73104977
lesterlagaras <63172017+lesterlagaras@users.noreply.github.com>
2026-03-20 16:10:05 +0800

    Delete navigation.js

--- navigation.js
+++
@@ -1,9 +0,0 @@
-// Sticky Navigation + Scroll Effect
-window.addEventListener('scroll', function() {
-    const nav = document.querySelector('nav');
-    if (window.scrollY > 50) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
-});
